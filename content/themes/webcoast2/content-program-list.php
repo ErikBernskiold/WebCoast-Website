ea401ffa6fee5dc4a9344851bdcce3a3195dfd8f
Erik Bernskiold <erik@bernskioldmedia.com>
2014-10-04 11:16:11 +0200

    Add twitter link if no other URL is present
    Signed-off-by: Erik Bernskiold <erik@bernskioldmedia.com>

--- content/themes/webcoast2/content-program-list.php
+++ content/themes/webcoast2/content-program-list.php
@@ -23,7 +23,22 @@
 		</li>
 		<li class="program-table-item-info small-24 medium-17 large-14 columns">
 			<div class="program-table-item-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
-			<div class="program-table-item-excerpt"><span class="program-table-item-speaker"><?php if(get_field('program_talare')) : $speaker_counter = 1; while(the_repeater_field('program_talare')) : ?><?php if( $speaker_counter > 1 ) : ?>, <?php endif; ?><a href="<?php the_sub_field('program_talare_url'); ?>"><?php the_sub_field('program_talare_namn'); ?></a><?php $speaker_counter++; endwhile; endif; ?></span> - <?php echo webcoast_get_excerpt( 25 ); ?></div>
+			<div class="program-table-item-excerpt">
+				<span class="program-table-item-speaker">
+					<?php if(get_field('program_talare')) : $speaker_counter = 1; while(the_repeater_field('program_talare')) : ?>
+						<?php if( $speaker_counter > 1 ) : ?>, <?php endif; ?>
+
+						<?php if ( get_sub_field( 'program_talare_url' ) ) : ?>
+							<a href="<?php the_sub_field('program_talare_url'); ?>">
+								<?php the_sub_field('program_talare_namn'); ?>
+							</a>
+						<?php else : ?>
+							<a href="https://twitter.com/<?php echo str_replace( '@', '', get_sub_field( 'program_talare_url' ) ); ?>">
+								<?php the_sub_field('program_talare_namn'); ?>
+							</a>
+						<?php endif; ?>
+					<?php $speaker_counter++; endwhile; endif; ?>
+				</span> - <?php echo webcoast_get_excerpt( 25 ); ?></div>
 		</li>
 		<li class="program-table-item-location small-24 medium-5 large-5 columns">
 			<?php $rooms = get_the_terms( $post->ID, 'webcoast_room' );
