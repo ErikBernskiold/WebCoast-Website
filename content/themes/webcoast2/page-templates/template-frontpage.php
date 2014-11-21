8e5d95ff99e61014fee4d466d298458944b0fc9c
Erik Bernskiold <erik@bernskioldmedia.com>
2014-11-21 10:31:21 +0100

    Current year in sponsor transient
    Signed-off-by: Erik Bernskiold <erik@bernskioldmedia.com>

--- content/themes/webcoast2/page-templates/template-frontpage.php
+++ content/themes/webcoast2/page-templates/template-frontpage.php
@@ -142,7 +142,7 @@ get_header(); ?>
 							),
 						);
 
-						$sponsor_query = webcoast_get_transient_query( 'wc_sp_' . $sponsor->slug . '_' . ICL_LANGUAGE_CODE , $sponsor_query_args, DAY_IN_SECONDS );
+						$sponsor_query = webcoast_get_transient_query( 'wc_sp_' . get_field('sponsor_display_year', 'option') . '_' . $sponsor->slug . '_' . ICL_LANGUAGE_CODE , $sponsor_query_args, DAY_IN_SECONDS );
 
 					if ( $sponsor_query->have_posts() ) : ?>
 					<article class="sponsor-row">
