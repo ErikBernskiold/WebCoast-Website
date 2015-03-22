bca342954f49470af3adc749db39de6f8ffe1213
Erik Bernskiold <erik@bernskioldmedia.com>
2015-03-22 15:19:44 +0100

    Related layout tweak
    Signed-off-by: Erik Bernskiold <erik@bernskioldmedia.com>

--- content/themes/webcoast2/yarpp-template-videos.php
+++ content/themes/webcoast2/yarpp-template-videos.php
@@ -3,16 +3,17 @@
 YARPP Template: Videos
 Author: Erik Bernskiold
 */
-?>
-<?php if (have_posts()):?>
-<h3 class="related-videos-title"><?php _e('Other interesting sessions', 'webcoast'); ?></h3>
-<div class="related-videos-container">
-	<?php while (have_posts()) : the_post(); ?>
 
-		<?php get_template_part('content', 'sessionvideo'); ?>
+if ( have_posts() ) :?>
 
-	<?php endwhile; ?>
-</div>
+	<h3 class="related-videos-title"><?php _e( 'Other interesting sessions', 'webcoast' ); ?></h3>
 
-<div class="clear"></div>
+	<div class="related-videos-container row">
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'content', 'sessionvideo' ); ?>
+
+		<?php endwhile; ?>
+	</div>
+
 <?php endif; ?>
